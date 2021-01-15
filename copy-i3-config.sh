b41917b965b849c99134311df7b5cb83254d115e
Andrej Holubek <andrej.holubek@my-rio.de>
2021-01-15 09:50:32 +0100

    update audio volume control

---
+++ copy-i3-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp /etc/i3blocks.conf ~/aho-projects/i3-setup/i3/i3blocks.conf
+cp -r ~/.config/i3 ~/aho-projects/i3-setup
+ls -lh ~/aho-projects/i3-setup
+ls -lh ~/aho-projects/i3-setup/i3/
+ls -lh ~/aho-projects/i3-setup/i3/bin
+ls -lh ~/aho-projects/i3-setup/i3/blocklets
